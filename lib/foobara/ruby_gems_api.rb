72579bd3a847596aebecbcda3bd39c79dd444bdc
Miles Georgi <azimux@gmail.com>
2024-11-08 19:06:07 -0800

    Extract generic HTTP Api behavior to an HttpApiGetCommand mixin

--- lib/foobara/ruby_gems_api.rb
+++ lib/foobara/ruby_gems_api.rb
@@ -1,3 +1,4 @@
 require "foobara/all"
 
+require_relative "../../src/foobara/http_api_get_command"
 Foobara::Util.require_directory "#{__dir__}/../../src"
--- src/foobara/ruby_gems_api/get_owners.rb
+++ src/foobara/ruby_gems_api/get_owners.rb
@@ -1,65 +1,15 @@
-require "uri"
-require "net/http"
-
 module Foobara
   module RubyGemsApi
     class GetOwners < Foobara::Command
-      API_URL = ->(gem_name) { "https://rubygems.org/api/v1/gems/#{gem_name}/owners.json" }
-
       inputs do
         gem_name :string, :required
       end
 
       result [User]
 
-      def execute
-        build_request_body
-        build_request_headers
-        issue_http_request
-        parse_response
-        build_gems
+      include HttpApiGetCommand
 
-        gems
-      end
-
-      attr_accessor :request_body, :request_headers, :response, :response_body, :gems
-
-      def build_request_body
-        self.request_body = {}
-      end
-
-      def build_request_headers
-        self.request_headers = {
-          "Content-Type" => "application/json"
-        }
-      end
-
-      def url
-        API_URL.call(gem_name)
-      end
-
-      def issue_http_request
-        uri = URI(url)
-        uri.query = URI.encode_www_form(request_body)
-        self.response = Net::HTTP.get_response(uri, request_headers)
-      end
-
-      def parse_response
-        json = if response.is_a?(Net::HTTPSuccess)
-                 response.body
-               else
-                 # :nocov:
-                 raise "Could not successfully hit #{url}: " \
-                       "#{response.code} #{response.message}"
-                 # :nocov:
-               end
-
-        self.response_body = JSON.parse(json)
-      end
-
-      def build_gems
-        self.gems = response_body
-      end
+      url { "https://rubygems.org/api/v1/gems/#{gem_name}/owners.json" }
     end
   end
 end
